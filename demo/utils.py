b3c10a1652bb9a208273f360597999f9e6a55a0e
Joe Yearsley <joe.yearsley@kheironmed.com>
2017-09-06 17:58:45 +0100

    Finsihed

--- demo/utils.py
+++ demo/utils.py
@@ -1,5 +1,4 @@
-from skimage import transform
-import caffe
+from skimage import transform, io, img_as_float
 import cv2
 import math
 import matplotlib.pyplot as plt
@@ -7,11 +6,20 @@ import numpy as np
 
 
 def imgPreprocess(img_path, size=224):
+    """
+    Process the image by removing the mean and reszing so that the smallest
+    edge is 256. Then take crops and average to predict.
+    :param img_path:
+    :param size:
+    :return:
+    """
     mean = np.array([103.939, 116.779, 123.68])
-    img = caffe.io.load_image(img_path)
+    img = img_as_float(io.imread(img_path)).astype(np.float32)
+    # Resize factor, smallest edge to 256.
     resFac = 256.0/min(img.shape[:2])
-    newSize = map(int, (img.shape[0]*resFac, img.shape[1]*resFac))
+    newSize = list(map(int, (img.shape[0]*resFac, img.shape[1]*resFac)))
     img = transform.resize(img, newSize, mode='constant')
+    # Calculate offsets of oversampling from caffe's oversample.
     if (size == 224):
         offset = [[0, 0], [0, newSize[1]-224], [newSize[0]-224, 0], [
                  newSize[0]-224, newSize[1]-224], [
@@ -30,14 +38,13 @@ def imgPreprocess(img_path, size=224):
 def pred(net, img):
     synsets = open('ilsvrc_synsets.txt').readlines()
     net.predict([img], oversample=True)
-    p = net.blobs['prob'].data
+    p = net.blobs['prob']
     # mean label
-    p_mean = np.argmax(np.mean(p, axis=0))
+    p_mean = np.argmax(p.mean(1))
     image_label = synsets[p_mean].split(',')[0].strip()
     image_label = image_label[image_label.index(' ')+1:]
-    # Individual labels of 5 crops
-    crop_labels = [np.argmax(p[0]), np.argmax(p[1]), np.argmax(p[2]),
-                   np.argmax(p[3]), np.argmax(p[4])]
+    # Individual labels of 5 crops (- ignore mirrored samples)
+    crop_labels = [np.argmax(p[i]) for i in range(5)]
     # To check if mean label matches any crop otherwise assign label of center
     if p_mean in crop_labels:
         p_m = p_mean
@@ -53,7 +60,7 @@ def pred(net, img):
     return points, image_label
 
 
-def out_rem(points, diag):
+def outlier_removal(points, diag):
     neighbors = np.zeros((points.shape[0]))
     selPoints = np.empty((1, 2))
     for i in range(points.shape[0]):
@@ -73,8 +80,10 @@ def heatmap(img, points, sigma=20):
     mask = np.zeros(img.shape[:2])
     shape = mask.shape
     for i in range(points.shape[0]):
+        # Check if inside the image
         if points[i, 0] < shape[0] and points[i, 1] < shape[1]:
             mask[points[i, 0], points[i, 1]] += 1
+    # Gaussian blur the points to get a nice heatmap
     blur = cv2.GaussianBlur(mask, (k, k), sigma)
     blur = blur*255/np.max(blur)
     return blur
@@ -86,7 +95,8 @@ def visualize(img_path, points, diag_percent, image_label):
     img = cv2.merge((r, g, b))
     diag = math.sqrt(img.shape[0]**2 + img.shape[1]**2)*diag_percent
     values = np.asarray(points)
-    selPoints = out_rem(values, diag)
+    selPoints = outlier_removal(values, diag)
+    # Make heatmap and show images
     hm = heatmap(np.copy(img), selPoints)
     _, ax = plt.subplots(1, 3, figsize=(15, 5))
     ax[0].imshow(img), ax[0].axis('off'), ax[0].set_title(image_label)
